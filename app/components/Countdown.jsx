0716280dd09808183c020508e3aaf11b1acf9f9c
Chris Davis <chris.davis@clearlink.com>
2017-03-31 13:42:50 -0600

    Add countdown functionality.

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -4,15 +4,38 @@ const CountdownForm = require('CountdownForm');
 
 const Countdown = React.createClass({
   getInitialState: function () {
-    return {count: 0};
+    return {
+      count: 0,
+      countdownStatus: 'stopped',
+    };
+  },
+  componentDidUpdate: function (prevProps, prevState) { // lifecycle
+    if (this.state.countdownStatus !== prevState.countdownStatus) { // if not 'stopped'
+      switch (this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+      }
+    }
+  },
+  startTimer: function () {
+    this.timer = setInterval(() => {
+      let newCount = this.state.count - 1;
+      this.setState({
+        count: newCount >= 0 ? newCount : 0 // ternary operator
+      });
+      
+      
+    }, 1000);
   },
   handleSetCountdown: function (seconds) {
     this.setState({
-      count: seconds
+      count: seconds,
+      countdownStatus: 'started',
     });
   },
   render: function() {
-    const {count} = this.state;
+    const {count, countdownStatus} = this.state;
     
     return (
       <div>
---
+++ app/tests/components/Countdown.test.jsx
@@ -0,0 +1,45 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const expect = require('expect');
+const $ = require('jQuery'); // as in webpack.config.js
+const TestUtils = require('react-addons-test-utils');
+
+const Countdown = require('Countdown');
+
+describe('Countdown', () => {
+  
+  // see if Countdown exists
+  it('should exist', () => {
+    expect(Countdown).toExist();
+  });
+  
+  describe('handleSetCountdown', () => {
+    it('should set state to started and count down', (done) => { // asynchronous test used
+      const countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      
+      countdown.handleSetCountdown(10);
+      
+      // assert
+      expect(countdown.state.count).toBe(10);
+      expect(countdown.state.countdownStatus).toBe('started');
+      
+      setTimeout(() => { // after a second, the count should be 9
+        expect(countdown.state.count).toBe(9);
+        done(); // this must be called because this is asynchronous
+      }, 1001);
+    });
+  
+    it('should never set count less than 0', (done) => { // asynchronous test used
+      const countdown = TestUtils.renderIntoDocument(<Countdown/>);
+    
+      countdown.handleSetCountdown(1);
+    
+      // assert
+      setTimeout(() => { // after a second, the count should be 9
+        expect(countdown.state.count).toBe(0);
+        done(); // this must be run because this is asynchronous
+      }, 3001);
+    });
+  });
+  
+});
